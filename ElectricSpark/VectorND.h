a01edd549c3bff9d66daf5b28e53eeace69746d2
bethanysimmons <bethanymsimmons@gmail.com>
2013-11-25 11:47:38 -0800

    Got Vector2D good.

---
+++ ElectricSpark/VectorND.h
@@ -0,0 +1,13 @@
+//
+//  VectorND.h
+//  ElectricSpark
+//
+//  Created by Bethany Simmons on 11/24/13.
+//  Copyright (c) 2013 Bethany Simmons. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VectorND : NSObject
++ (CGFloat)dotProduct:(VectorND *)aVector;
+@end
